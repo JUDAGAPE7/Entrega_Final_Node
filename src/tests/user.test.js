54c593fadac13bde20e675778b53a7254c7aa8bd
JUDAGAPE7 <juandagacha@hotmail.com>
2024-08-22 00:43:37 -0500

    we are missing update & delete

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -4,7 +4,7 @@ const app = require('../app')
 
 const BASE_URL = '/api/v1/users'
 let TOKEN
-
+let TOKEN2
 
 beforeAll(async () => {
     
@@ -17,8 +17,8 @@ beforeAll(async () => {
     .send(user)
 
     TOKEN = res.body.token
-
-    console.log(TOKEN);
+    
+    //console.log(TOKEN);
     
 })
 
@@ -50,3 +50,50 @@ test('POST -> BASE_URL, should return statusCode 201, and res.body.firstName ===
     });
 
  })
+
+ test('GET -> BASE_URL. should return statusCode 200 and res.body.lenght === 2', async() => { 
+
+    const res = await request(app)
+    .get(BASE_URL)
+    .set('Authorization', `Bearer ${TOKEN}`)
+    
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveLength(2)
+  })
+
+
+  test('POST ->"BASE_URL/LOGIN", should return status code 200 and res,body.user.email === user.email', async () => { 
+
+    const user = {
+        email: "maria@hotmail.com",
+        password: "maria1234"
+    } 
+
+
+    const res = await request(app)
+    .post(`${BASE_URL}/login`)
+    .send(user)
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toBeDefined()
+
+console.log(res.body.user);
+
+    expect(res.body.user.email).toBe(user.email)
+
+   })
+
+
+
+
+//    test('PUT -> "', () => {    const user = {
+//     email: user.email,
+//     password: user.password
+// }
+
+// const res = request(app)
+// .post(`${BASE_URL}/login`)
+// .send(user) 
+
+//  })
