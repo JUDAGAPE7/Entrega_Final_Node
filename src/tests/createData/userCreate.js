bbddace56ae0abcfa0ac6a1cd0adbc42794ecb89
JUDAGAPE7 <juandagacha@hotmail.com>
2024-08-21 23:04:33 -0500

    iniciando testing

---
+++ src/tests/createData/userCreate.js
@@ -0,0 +1,20 @@
+const User = require("../../models/User")
+
+
+const userCreate = async () => {
+    
+const user ={
+    firstName: "Pedro",
+    lastName: "Pazcal",
+    email: "pedro@hotmail.com",
+    password: "pedro1234",
+    phone: "+573203204040",
+}
+
+
+    await User.create(user)
+}
+
+
+
+module.exports =userCreate
