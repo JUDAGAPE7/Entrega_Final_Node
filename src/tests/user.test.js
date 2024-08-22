5eb7c6ecd6c885241883e9467df2235866674360
JUDAGAPE7 <juandagacha@hotmail.com>
2024-08-22 00:01:30 -0500

    capture token

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,4 +1,26 @@
-const User = require("../models/User")
+
+const request = require('supertest')
+const app = require('../app')
+
+const BASE_URL = '/api/v1/users'
+let TOKEN
+
+
+beforeAll(async () => {
+    
+    const user ={
+    email: "pedro@hotmail.com",
+    password: "pedro1234",
+    } 
+    const res = await request(app)
+    .post(`${BASE_URL}/login`)
+    .send(user)
+
+    TOKEN = res.body.token
+
+    console.log(TOKEN);
+    
+})
 
 
 const user ={
@@ -10,13 +32,21 @@ const user ={
 }
 
 
-const BASE_URL = '/api/v1/users'
 
 
-test('POST -> BASE_URL, should return statusCode 201, and res.body.firstName === user.firstName', async () => { 
+test('POST -> BASE_URL, should return statusCode 201, and res.body.firstName === user.firstName', async () => {  
+
+    const userDetails= [ 'firstName', 'lastName', 'email',  'phone' ]
     
-    const user = await User.findAll()
-    console.log(user);
+    const res = await request(app)
+    .post(BASE_URL)
+    .send(user)
     
+
+    expect(res.statusCode).toBe(201)
+    expect(res.body).toBeDefined()
+    userDetails.forEach( userDetail => {
+    return    expect(res.body[userDetail]).toBe(user[userDetail])
+    });
 
  })
