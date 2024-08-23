378aa02615e8aafba705e401f271a03f1bfc763a
JUDAGAPE7 <juandagacha@hotmail.com>
2024-08-22 20:52:55 -0500

    category&products

---
+++ src/tests/category.test.js
@@ -0,0 +1,55 @@
+const request = require('supertest')
+const app = require('../app')
+
+
+const BASE_URL = '/api/v1/cotegories'
+const BASE_URL_LOGIN = '/api/v1/login'
+
+let categoryId
+
+beforeAll(async() => {
+    const user = {
+        email: "pedro@hotmail.com",
+        password: "pedro1234"
+    }
+    const res = await request(app)
+        .post(BASE_URL_LOGIN)
+        .send(user)
+    TOKEN = res.body.token
+})
+
+const category = {
+    name: "Sweater"
+}
+
+test("POST --> BASE_URL, should return statusCode 201, and res.body.name === category.name", async() => {
+    const res = await request(app)
+        .post(BASE_URL)
+        .send(category)
+        .set('Authorization', `Bearer ${TOKEN}`)
+        // console.log(res.body)
+        categoryId = res.body.id;
+        expect(res.statusCode).toBe(201)
+        expect(res.body).toBeDefined()
+        expect(res.body.name).toBeDefined()
+        expect(res.body.name).toBe(category.name)
+})
+
+
+test('GET -> BASE_URL. should return statusCode 200 and res.body.lenght === 2', async() => { 
+
+    const res = await request(app)
+    .get(BASE_URL)
+    .set('Authorization', `Bearer ${TOKEN}`)
+    
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveLength(1)
+  })
+
+  test("DELETE -> BASE_URL/categoryId, should return statusCode 204, and res.body.name === category.name", async () => {
+    const res = await request(app)
+      .delete(`${BASE_URL}/${categoryId}`)
+      .set('Authorization', `Bearer ${TOKEN}`)
+    expect(res.statusCode).toBe(204)
+  })
--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -6,6 +6,8 @@ const BASE_URL = '/api/v1/users'
 let TOKEN
 let TOKEN2
 
+let userID
+
 beforeAll(async () => {
     
     const user ={
@@ -42,6 +44,7 @@ test('POST -> BASE_URL, should return statusCode 201, and res.body.firstName ===
     .post(BASE_URL)
     .send(user)
     
+    userID = res.body.id
 
     expect(res.statusCode).toBe(201)
     expect(res.body).toBeDefined()
@@ -87,13 +90,27 @@ console.log(res.body.user);
 
 
 
-//    test('PUT -> "', () => {    const user = {
-//     email: user.email,
-//     password: user.password
-// }
+   test('PUT -> "BASE_URL/userI" should return statusCode 201, and res.body.user === userUpdate.firstName', async () => {    
+    
+    const userUpdate = {
+        firstName: "Isabel",
+        }
 
-// const res = request(app)
-// .post(`${BASE_URL}/login`)
-// .send(user) 
+        const res = await request(app)
+        .put(`${BASE_URL}/${userID}`)
+        .send(userUpdate)
+        .set('Authorization', `Bearer ${TOKEN}`)
+    
+      expect(res.statusCode).toBe(200)
+      expect(res.body).toBeDefined()
+      expect(res.body.firstName).toBe(userUpdate.firstName)
 
-//  })
+ })
+
+
+ test("DELETE -> BASE_URL/userID, should return statusCode 204, and res.body.firstName === category.firstName", async () => {
+    const res = await request(app)
+      .delete(`${BASE_URL}/${userID}`)
+      .set('Authorization', `Bearer ${TOKEN}`)
+    expect(res.statusCode).toBe(204)
+  })
