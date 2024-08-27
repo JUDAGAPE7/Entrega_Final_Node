952658dde8082ae4bd5da258dce56d0bc479ca92
JUDAGAPE7 <juandagacha@hotmail.com>
2024-08-26 22:24:13 -0500

    category getsall and One

--- src/models/index.js
+++ src/models/index.js
@@ -1,3 +1,4 @@
+const Cart = require("./Cart");
 const Category = require("./Category");
 const Product = require("./Product");
 const User = require("./User");
@@ -6,3 +7,14 @@ const User = require("./User");
     // Product --> categoryId
     Product.belongsTo(Category)
     Category.hasMany(Product)
+
+
+   // Cart -> USerId
+    Cart.belongsTo(User)
+    User.hasMany(Cart)
+
+
+     //Cart -> ProductId
+
+     Cart.belongsTo(Product)
+     Product.hasMany(Cart)
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const routerUser = require('./user.router');
 const routerProduct = require('./product.router');
 const routerCategory = require('./category.router');
+const { verifyJWT } = require('../utils/verifyJWT');
+const routerCart = require('./cart.router');
 const router = express.Router();
 
 // colocar las rutas aquí
 router.use('/users',routerUser)
 router.use("/categories", routerCategory)
 router.use('/products',routerProduct)
+router.use('/cart', verifyJWT, routerCart) //🔏
 
 module.exports = router;
 
